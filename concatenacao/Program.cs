ff19647e5c1924fb2b7c083a7fa4c23ec1c846fa
LorisKizy <lorenakizyzanoski@gmail.com>
2024-11-04 17:24:06 -0400

    adicionar concatenacao

--- concatenacao/Program.cs
+++ concatenacao/Program.cs
@@ -1,6 +1,24 @@
-﻿// Concatenacao coisa + coisa + coisa
-
+﻿// Concatenacao "coisa" + "coisa" + "coisa"
 string nome = "Joao";
 string saudacao = "Ola " + nome +"! Bem-vindo.";
 Console.WriteLine(saudacao);
 
+// Interpolacao $ "coisa {} coisa"
+
+string nome = "Joao";
+string saudacao1 = $"Ola,{nome}! Bem Vindo.";
+Console.WriteLine(saudacao1);
+
+
+// (coisa.GetType())
+int numero = 5;
+Console.WriteLine(numero.GetType());
+
+// Palavra-Chave typeof
+Console.WriteLine(typeof(int));
+
+//Metodo parse
+string valorTexto = "100";
+Console.WriteLine("A variavel valorTexto e do tipo: " + valorTexto.GetType());
+
+//conversao de string para numero apenas dos tipos primitivos
