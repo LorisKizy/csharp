ca9311ef0ac46f2ad4d4a8edebcf18411f2435b0
LorisKizy <lorenakizyzanoski@gmail.com>
2024-11-04 15:24:56 -0400

    adicionado operadores logicos

---
+++ operadores/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//operadores logicos
+//n pode ser nome de variavel igual (ex.: a em todos os operadores)
+//E logico &&
+bool a = true;
+bool b = false;
+Console.WriteLine(a&&b);
+
+//NOT logico !
+bool a1 = true;
+Console.WriteLine(!a1);
+
+//OU logico ||
+bool a2 = true;
+bool b2 = false;
+Console.WriteLine(a2||b2);
+
+//XOR logico ^ (OU Exclusivo)
+bool c = true;
+bool d = false;
+Console.WriteLine(c^d);
+
+//Precedencia ordem de operacoes logicas
+
+bool e = true;
+bool f = false;
+bool g = false;
+bool resultado = !e || (f&&g);
+Console.WriteLine(resultado);
+
